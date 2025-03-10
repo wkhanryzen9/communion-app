bc0a285a7187361d72378ae56fbd8480e0a7d87a
wkhanryzen9 <wkhan5563@gmail.com>
2025-03-11 01:07:41 +0530

    minor update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <section style={{ textAlign: "center", padding: "50px" }}>
-        <h1>Connecting People Across Faiths & Interests</h1>
-        <p>Connecting people of all faiths through events and community support.</p>
+      <section style={{height: '90vh'}} className="w-full flex justify-center items-center gap-2 flex-col">
+        <h1 className="text-4xl">Connecting People Across Faiths & Interests</h1>
+        <p className="text-xl">Connecting people of all faiths through events and community support.</p>
         <Link to="/events">
-          <button>Explore Events</button>
+          <button className="text-lg text-white font-semibold px-5 py-2 bg-emerald-700 rounded mt-4">Explore Events</button>
         </Link>
       </section>
     </div>
